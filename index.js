bfcd32a3f718f57612b5ca549467525dd24e57c7
Ishtiaq Ahmad <rao.ishtyaq@gmail.com>
2021-08-16 16:24:34 +0400

    My second commit

--- index.js
+++ index.js
@@ -9,19 +9,7 @@ app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
  
-//app.use(express.json());
-const model = new TeachableMachine({
-    modelUrl: "https://teachablemachine.withgoogle.com/models/r6BBk-hiN/"
-  });
-  
-  model.classify({
-    imageUrl: "https://media-blog.sashido.io/content/images/2020/09/SashiDo_Dog.jpg",
-  }).then((predictions) => {
-    console.log("Predictions:", predictions);
-  }).catch((e) => {
-    console.log("ERROR", e);
-  });
-  
+
 var routes = require('./api/routes/documentRoutes'); //importing route
 routes(app); //register the route
 // app.use("/orders", orderRoutes);
